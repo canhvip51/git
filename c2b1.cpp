93ac22570fe01201e9c2086e81dffef9ef6d399d
DESKTOP-VES4POV\ADMIN <canhvip51@gmail.com>
2020-10-02 00:30:21 +0700

    update

--- c2b1.cpp
+++ c2b1.cpp
@@ -8,26 +8,23 @@ double f(double a,double b,double c,double d, double x){
 	return a*pow(x,3)+b*pow(x,2)+c*x+d;
 }
 double ng(double a,double b,double c,double d, double x, double y){
-	cout<<x<<endl;
-	cout<<y<<endl;
-	if(f(a,b,c,d,x)==0)
-	return x;
-	if(f(a,b,c,d,y)==0)
-	return y;
-	if(f(a,b,c,d,x)!=0||f(a,b,c,d,y)!=0){
-			if(f(a,b,c,d,x)*f(a,b,c,d,(x+y)/2)>0){
-				cout<<(x+y)/2<<endl;
-				return ng(a,b,c,d,(x+y)/2,y);
-			}
-			 
+	if(f(a,b,c,d,x)*f(a,b,c,d,y)==0){
+		if(f(a,b,c,d,x)==0)
+		return x;
+		if(f(a,b,c,d,y)==0)
+		return y;
+	}	
+	else if(f(a,b,c,d,x)*f(a,b,c,d,y)!=0){
+		if(f(a,b,c,d,x)*f(a,b,c,d,(x+y)/2)>0){
+		cout<<"x: "<<(x+y)/2<<endl;
+		return ng(a,b,c,d,(x+y)/2,y);
+	}
 		else if(f(a,b,c,d,y)*f(a,b,c,d,(x+y)/2)>0)
 		{
-				cout<<(x+y)/2<<endl;
-				return ng(a,b,c,d,x,(x+y)/2);
-			}
-			
+			cout<<"y: "<<(x+y)/2<<endl;
+			return ng(a,b,c,d,x,(x+y)/2);
+		}
 	}
-	
 }
 int main(){
 cout<<ng(1,1,1,1,-2,1);
