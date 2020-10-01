a17526b55f3b64f70b50e2e29ac3ca0f4d85f2f3
DESKTOP-VES4POV\ADMIN <canhvip51@gmail.com>
2020-10-02 00:14:24 +0700

    update c2b1

---
+++ c2b1.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+#include<algorithm>
+#include<cmath>
+using namespace std;
+double f(double a,double b,double c,double d, double x){
+	return a*pow(x,3)+b*pow(x,2)+c*x+d;
+}
+double ng(double a,double b,double c,double d, double x, double y){
+	cout<<x<<endl;
+	cout<<y<<endl;
+	if(f(a,b,c,d,x)==0)
+	return x;
+	if(f(a,b,c,d,y)==0)
+	return y;
+	if(f(a,b,c,d,x)!=0||f(a,b,c,d,y)!=0){
+			if(f(a,b,c,d,x)*f(a,b,c,d,(x+y)/2)>0){
+				cout<<(x+y)/2<<endl;
+				return ng(a,b,c,d,(x+y)/2,y);
+			}
+			 
+		else if(f(a,b,c,d,y)*f(a,b,c,d,(x+y)/2)>0)
+		{
+				cout<<(x+y)/2<<endl;
+				return ng(a,b,c,d,x,(x+y)/2);
+			}
+			
+	}
+	
+}
+int main(){
+cout<<ng(1,1,1,1,-2,1);
+return 0;
+}
