67594b53494eb7313109b2f8cefbd5897e8a1f87
DESKTOP-VES4POV\ADMIN <canhvip51@gmail.com>
2020-10-02 22:15:41 +0700

    update

--- c2b1.cpp
+++ c2b1.cpp
@@ -5,28 +5,28 @@
 #include<cmath>
 using namespace std;
 double f(double a,double b,double c,double d, double x){
-	return a*pow(x,3)+b*pow(x,2)+c*x+d;
+	return roundf((a*pow(x,3)+b*pow(x,2)+c*x+d) * 100000) / 100000 ;
 }
 double ng(double a,double b,double c,double d, double x, double y){
-	if(f(a,b,c,d,x)*f(a,b,c,d,y)==0){
-		if(f(a,b,c,d,x)==0)
-		return x;
-		if(f(a,b,c,d,y)==0)
-		return y;
+	if(roundf(f(a,b,c,d,x)* 10000) / 10000*roundf(f(a,b,c,d,y)* 10000)/ 10000==0){
+		if(roundf(f(a,b,c,d,x)* 10000) / 10000==0)
+		return x ;
+		if(roundf(f(a,b,c,d,y)* 10000)/ 10000==0)
+		return y ;
 	}	
 	else if(f(a,b,c,d,x)*f(a,b,c,d,y)!=0){
 		if(f(a,b,c,d,x)*f(a,b,c,d,(x+y)/2)>0){
-		cout<<"x: "<<(x+y)/2<<endl;
-		return ng(a,b,c,d,(x+y)/2,y);
+			x=roundf((x+y)/2 * 100000) / 100000 ;
+		return ng(a,b,c,d,x,y);
 	}
 		else if(f(a,b,c,d,y)*f(a,b,c,d,(x+y)/2)>0)
 		{
-			cout<<"y: "<<(x+y)/2<<endl;
-			return ng(a,b,c,d,x,(x+y)/2);
+			y=roundf((x+y)/2 * 100000) / 100000 ;
+			return ng(a,b,c,d,x,y);
 		}
 	}
 }
 int main(){
-cout<<ng(1,1,1,1,-2,1);
+cout<<ng(1,2,3,4,-5,5);
 return 0;
 }
