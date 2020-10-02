039159fb0aba8667d38260bec7956a464c29f57d
DESKTOP-VES4POV\ADMIN <canhvip51@gmail.com>
2020-10-02 22:24:39 +0700

    update

--- c2b1.cpp
+++ c2b1.cpp
@@ -8,10 +8,10 @@ double f(double a,double b,double c,double d, double x){
 	return roundf((a*pow(x,3)+b*pow(x,2)+c*x+d) * 100000) / 100000 ;
 }
 double ng(double a,double b,double c,double d, double x, double y){
-	if(roundf(f(a,b,c,d,x)* 10000) / 10000*roundf(f(a,b,c,d,y)* 10000)/ 10000==0){
-		if(roundf(f(a,b,c,d,x)* 10000) / 10000==0)
+	if(roundf(f(a,b,c,d,x)* 1000) / 1000*roundf(f(a,b,c,d,y)* 1000)/ 1000==0){
+		if(roundf(f(a,b,c,d,x)* 1000) / 1000==0)
 		return x ;
-		if(roundf(f(a,b,c,d,y)* 10000)/ 10000==0)
+		if(roundf(f(a,b,c,d,y)* 1000)/ 1000==0)
 		return y ;
 	}	
 	else if(f(a,b,c,d,x)*f(a,b,c,d,y)!=0){
@@ -27,6 +27,6 @@ double ng(double a,double b,double c,double d, double x, double y){
 	}
 }
 int main(){
-cout<<ng(1,2,3,4,-5,5);
+cout<<ng(9,-4,2,10,-100,100);
 return 0;
 }
