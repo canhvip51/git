a1ed417ce2173ee73d60a5d03f144e3f597613a0
DESKTOP-VES4POV\ADMIN <canhvip51@gmail.com>
2020-10-08 10:15:51 +0700

    update

--- btphantich.cpp
+++ btphantich.cpp
@@ -2,24 +2,36 @@
 using namespace std;
 int const MAX=100;
 int a[MAX],n;
-int sum=0;
+int sum=0,vt=0;
 void print(int x){
 	for( int i=1;i<x;i++)
 	cout<<a[i]<<" ";
+	cout<<endl;
 }
 void gen(int k){
 	
-	if(sum==4)
+	if(sum==n||k>n)
 	{
+		if(sum==n)
 		print(k);
+		sum-=a[vt];
+		vt--;
 		return;
 	}
 	for(int i=1; i<=n;i++){
 		a[k]=i;
-		sum+=a[i];
-		if(sum>4)return;
+		if(sum+a[k]>n){
+			
+		break;
+		}
+		sum+=a[k];
+		vt++;
 		gen(k+1);
 	}
+	
+	sum-=a[vt];
+	vt--;
+	return;
 }
 int main(){
 	n=4;
